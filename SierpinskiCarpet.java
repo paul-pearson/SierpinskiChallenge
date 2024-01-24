c2cf176eb01f38e3d95e8ea9d15243090be84c7f
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2024-01-24 13:27:27 +0000

    Update SierpinskiCarpet.java

--- SierpinskiCarpet.java
+++ SierpinskiCarpet.java
@@ -1,15 +1,10 @@
 public class SierpinskiCarpet {
 
-    /**
-     * Draws a Sierpinski carpet of order n to the console.
-     *
-     * @param carpet the 2D character array representing the carpet
-     * @param x      the x-coordinate of the top-left corner of the carpet
-     * @param y      the y-coordinate of the top-left corner of the carpet
-     * @param size   the size of the carpet
-     */
-    public static void drawCarpet(char[][] carpet, int x, int y, int size) {
-        // TODO: Implement this method
+   public static void main(String[] args) {
+        drawSierpinskiCarpet(3, 27);
     }
-
+    
+    public static void drawSierpinskiCarpet(int order, int size) {
+        //TODO: Implement this 
+    }
 }
