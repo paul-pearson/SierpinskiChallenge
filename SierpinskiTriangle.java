8d894e583ac4630f7f4313aad04f6ef87582efb5
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2024-01-24 13:45:27 +0000

    Update SierpinskiTriangle.java

--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -1,7 +1,12 @@
 public class SierpinskiTriangle {
     public static void main(String[] args) {
+        drawTriangle(4)
+
         drawSierpinskiTriangle(3);
     }
+    public static void drawTriangle(int height){
+        //TODO: Implement this 
+    };
     
     public static void drawSierpinskiTriangle(int order) {
      //TODO: Implement this   
