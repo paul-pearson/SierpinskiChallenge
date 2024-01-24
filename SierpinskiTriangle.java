ef54e304f9634f67f50735ac79c33e829dcd0242
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2024-01-24 13:28:57 +0000

    Update SierpinskiTriangle.java

--- SierpinskiTriangle.java
+++ SierpinskiTriangle.java
@@ -1,22 +1,9 @@
 public class SierpinskiTriangle {
-    /**
-     * Draws a Sierpinski triangle of order n to the console.
-     *
-     * @param n the order of the triangle
-     */
-    public static void drawTriangle(int n) {
-        // TODO: Implement this method
+    public static void main(String[] args) {
+        drawSierpinskiTriangle(3);
     }
-
-    /**
-     * Draws a Sierpinski triangle of order n to the console.
-     *
-     * @param triangle the 2D character array representing the triangle
-     * @param x        the x-coordinate of the top-left corner of the triangle
-     * @param y        the y-coordinate of the top-left corner of the triangle
-     * @param size     the size of the triangle
-     */
-    public static void drawTriangleAnyN(char[][] triangle, int x, int y, int size) {
-        // TODO: Implement this method
+    
+    public static void drawSierpinskiTriangle(int order) {
+     //TODO: Implement this   
     }
 }
