2399ac5a3926bc252c63ca8d7b53d74e538c3f17
paul-pearson <paul.pearson@version1.com>
2024-01-26 12:29:53 +0000

    Continue carpet impl

--- src/main/java/Carpet.java
+++ src/main/java/Carpet.java
@@ -4,6 +4,7 @@ public class Carpet {
 
 
     private char STAR = '*';
+    private char SPACE = ' ';
 
     private final char[][] array;
     private int order;
@@ -18,13 +19,35 @@ public class Carpet {
 
 
     public void generate() {
-        removeSquares(size / 3, 2  * size/ 3, size / 3, 2 * size / 3);
+        int currentOrder = 1;
+        int squareRow = 0;
+        int squareColumn = 0;
+        int squareSize = size;
+        doRecursion(currentOrder, squareRow, squareColumn, squareSize);
+        //removeSquares(size / 3, 2  * size/ 3, size / 3, 2 * size / 3);
         new ArrayDrawer().drawArray(array);
     }
 
-    private void removeSquares(int startRow, int endRow, int startColumn, int endColumn) {
-        for (int row = startRow; row < endRow; row++) {
+    private void doRecursion(int currentOrder, int squareRow, int squareColumn, int squareSize) {
+        if (currentOrder <= order) {
+            int startRow = squareSize / 3 + squareRow;
+            int startColumn = squareSize / 3 + squareColumn;
+            int endRow = startRow + squareSize / 3;
+            int endColumn = startColumn + squareSize / 3;
+            removeSquares(startRow, endRow, startColumn, endColumn);
+            doRecursion(currentOrder + 1, squareRow, squareColumn, squareSize / 3);
+            doRecursion(currentOrder + 1, squareRow, squareColumn + squareSize / 3, squareSize / 3);
+            doRecursion(currentOrder + 1, squareRow, squareColumn + 2 * squareSize / 3, squareSize / 3);
+        }
 
+    }
+
+    private void removeSquares(int startRow, int endRow, int startColumn, int endColumn) {
+        for (int rowNum = startRow; rowNum < endRow; rowNum++) {
+            char[] row = array[rowNum];
+            for (int columnNum = startColumn; columnNum < endColumn; columnNum++) {
+                row[columnNum] = SPACE;
+            }
         }
     }
 }
